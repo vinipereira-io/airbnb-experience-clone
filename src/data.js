5736c8db2170b317465c70ad796e8ff50390079c
Vini Pereira <vini@vinipereira.io>
2023-02-24 20:43:15 +1100

    display images in the cards

--- src/data.js
+++ src/data.js
@@ -1,10 +1,14 @@
+import Katie from "media/katie-zaferes.png"
+import Wedding from "media/wedding-photography.png"
+import MountainBiking from "media/mountain-bike.png"
+
 export default [
     {
         id: 1,
         title: "Life Lessons with Katie Zaferes",
         description: "I will share with you what I call \"Positively Impactful Moments of Disappointment.\" Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.",
         price: 136,
-        coverImg: "katie-zaferes.png",
+        coverImg: Katie,
         stats: {
             rating: 5.0,
             reviewCount: 6
@@ -17,7 +21,7 @@ export default [
         title: "Learn Wedding Photography",
         description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
         price: 125,
-        coverImg: "wedding-photography.png",
+        coverImg: Wedding,
         stats: {
             rating: 5.0,
             reviewCount: 30
@@ -30,7 +34,7 @@ export default [
         title: "Group Mountain Biking",
         description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
         price: 50,
-        coverImg: "mountain-bike.png",
+        coverImg: MountainBiking,
         stats: {
             rating: 4.8,
             reviewCount: 2
